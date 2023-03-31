019dc27c470d606f343572a8d7c03f2d83bd3cf2
Chau Tran <chautran@Chaus-MacBook-Pro.local>
2023-03-31 15:43:42 -0500

    clean up

--- src/app/scene/github-users.ts
+++ src/app/scene/github-users.ts
@@ -1,5 +1,2 @@
-export const users = [
-    ['nartc', 1],
-    ['spencercope', 2],
-    ['mlbc95', 3],
-];
+// add your Github Username and a number from 0 -> 8
+export const users = [['nartc', 1]];
